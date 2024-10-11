0a791e0d6ead96f9fd1bdb5ebc970762926ece1a
hsibtain18 <33388697+hsibtain18@users.noreply.github.com>
2024-10-11 19:32:21 +0500

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { getDashboardMetrics } from "./controllers/dashboardController";
+import dashboardRoutes from "./routes/dashboardRoutes";
+import expenseRoutes from "./routes/expenseRoutes";
+import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
+ 
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+ 
+app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
+app.use("/products", productRoutes); // http://localhost:8000/products
+app.use("/users", userRoutes); // http://localhost:8000/users
+app.use("/expenses", expenseRoutes); // http://localhost:8000/expenses
+
+/* SERVER */
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server as running ossn port ${port}`);
+});
